733aed79c19158575cc82bf306b0aa85f6b72499
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2023-07-19 11:04:23 -0400

    Implemented tests for FiniteElementPatch.

--- src/vegapunk/__init__.py
+++ src/vegapunk/__init__.py
@@ -1,1 +1,16 @@
+#
+#                                                                       Modules
+# =============================================================================
+import sys
+import pathlib
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Add project root directory to sys.path
+root_dir = str(pathlib.Path(__file__).parents[0])
+if root_dir not in sys.path:
+    sys.path.insert(0, root_dir)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from src.vegapunk import patch
+from src.vegapunk import patch_generator
+from src.vegapunk import finite_element
+from src.vegapunk import patch_dataset
 
