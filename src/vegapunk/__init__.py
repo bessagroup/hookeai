ec2f2107700737a507800fbc371543b49a2bc62d
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2023-09-27 17:09:59 -0400

    Add missing imports and enclose main module.

--- src/vegapunk/__init__.py
+++ src/vegapunk/__init__.py
@@ -8,4 +8,9 @@ import pathlib
 root_dir = str(pathlib.Path(__file__).parents[0])
 if root_dir not in sys.path:
     sys.path.insert(0, root_dir)
-
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from vegapunk import main
+from vegapunk import material_patch
+from vegapunk import gnn_model
+from vegapunk import ioput
+from vegapunk import simulators
