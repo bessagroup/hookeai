e978f8f449997919e1d4008230a2dfd98d633981
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2023-09-29 18:50:42 -0400

    Handling modules imports.

--- src/vegapunk/__init__.py
+++ src/vegapunk/__init__.py
@@ -9,8 +9,8 @@ root_dir = str(pathlib.Path(__file__).parents[0])
 if root_dir not in sys.path:
     sys.path.insert(0, root_dir)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-from vegapunk import main
-from vegapunk import material_patch
-from vegapunk import gnn_model
-from vegapunk import ioput
-from vegapunk import simulators
+from src.vegapunk import main
+from src.vegapunk import material_patch
+from src.vegapunk import gnn_model
+from src.vegapunk import ioput
+from src.vegapunk import simulators
---
+++ src/vegapunk/user_scripts/train_material_patch_model.py
@@ -0,0 +1,24 @@
+"""User script: Train Graph Neural Network based material patch model."""
+#
+#                                                                       Modules
+# =============================================================================
+# Standard
+import sys
+import pathlib
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Add project root directory to sys.path
+root_dir = str(pathlib.Path(__file__).parents[1])
+if root_dir not in sys.path:
+    sys.path.insert(0, root_dir)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Third-party
+import numpy as np
+# Local
+
+#
+#                                                          Authorship & Credits
+# =============================================================================
+__author__ = 'Bernardo Ferreira (bernardo_ferreira@brown.edu)'
+__credits__ = ['Bernardo Ferreira', ]
+__status__ = 'Planning'
+# =============================================================================
