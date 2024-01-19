210b7cb5b0f1ed5b36da762692aef7ca7093fbce
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2024-01-19 12:31:23 -0500

    Fix small bug in user script.

--- src/vegapunk/projects/gnn_material_patch/optimization/hydra_optimization.py
+++ src/vegapunk/projects/gnn_material_patch/optimization/hydra_optimization.py
@@ -17,7 +17,7 @@ import sys
 import pathlib
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Add project root directory to sys.path
-root_dir = str(pathlib.Path(__file__).parents[2])
+root_dir = str(pathlib.Path(__file__).parents[3])
 if root_dir not in sys.path:
     sys.path.insert(0, root_dir)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
