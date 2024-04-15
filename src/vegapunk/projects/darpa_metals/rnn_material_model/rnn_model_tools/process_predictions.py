12f4f1a0feac55dcf65cf0eaaf97c0f36d7cb39f
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2024-04-15 14:29:42 -0400

    Implemented the building of time series prediction and ground-truth data arrays.

--- src/vegapunk/projects/darpa_metals/rnn_material_model/rnn_model_tools/process_predictions.py
+++ src/vegapunk/projects/darpa_metals/rnn_material_model/rnn_model_tools/process_predictions.py
@@ -14,6 +14,7 @@ import re
 # Third-party
 import numpy as np
 # Local
+from rnn_base_model.data.time_dataset import load_dataset
 from gnn_base_model.predict.prediction import load_sample_predictions
 #
 #                                                          Authorship & Credits
@@ -138,11 +139,144 @@ def build_prediction_data_arrays(predictions_dir, prediction_type,
                 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                 # Build sample data array
                 data_array = np.concatenate(
-                        (stress_path[:, i].reshape((-1, 1)),
-                         stress_path_target[:, i].reshape((-1, 1))), axis=1)
+                    (stress_path_target[:, i].reshape((-1, 1)),
+                     stress_path[:, i].reshape((-1, 1))), axis=1)
             # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             # Assemble sample prediction data
             prediction_data_arrays[i] = \
                 np.append(prediction_data_arrays[i], data_array, axis=0)
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     return prediction_data_arrays
+# =============================================================================
+def build_time_series_predictions_data(dataset_file_path, predictions_dir,
+                                       prediction_type, samples_ids='all'):
+    """Build times series prediction and ground-truth data arrays.
+    
+    Parameters
+    ----------
+    dataset_file_path : str
+        Time series testing data set file path.
+    predictions_dir : str
+        Directory where samples predictions results files are stored.
+    prediction_type : {'stress_comps_paths',}
+        Type of prediction data arrays:
+        
+        'stress_comps_paths' : Stress components paths
+
+    samples_ids : {'all', list[int]}, default='all'
+        Samples for which the data arrays with the time series prediction
+        and ground-truth are built.
+    
+    Returns
+    -------
+    prediction_data_dicts : list[dict]
+        Prediction components data arrays. Each prediction component is stored
+        as a dictionary, where the data array (item, np.ndarray(2d)) of each
+        sample (key, str) is stored as a numpy.ndarray(2d) of shape
+        (n_nodes, 3), where data_array[i, 0] stores the time series discrete
+        time, data_array[i, 0] stores the time series ground-truth and
+        data_array[i, 1] stores the time series prediction prediction.
+    """
+    # Load testing data set
+    test_dataset = load_dataset(dataset_file_path)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Check sample predictions directory
+    if not os.path.isdir(predictions_dir):
+        raise RuntimeError('The samples predictions directory has not been '
+                           'found:\n\n' + predictions_dir)
+    # Check samples IDs
+    if samples_ids != 'all' and not isinstance(samples_ids, list):
+        raise RuntimeError('Samples IDs must be specified as "all" or as '
+                           'list[int].')
+    elif (isinstance(samples_ids, list)
+          and not all([isinstance(x, int) for x in samples_ids])):
+        raise RuntimeError('Samples IDs must be specified as a list of '
+                           'integers.')
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Get files in samples predictions results directory
+    directory_list = os.listdir(predictions_dir)
+    # Check directory
+    if not directory_list:
+        raise RuntimeError('No files have been found in directory where '
+                           'samples predictions results files are stored.')
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Get prediction files samples IDs
+    prediction_files_ids = []
+    for filename in directory_list:
+        # Check if file is sample results file
+        id = re.search(r'^prediction_sample_([0-9]+).pkl$', filename)
+        # Assemble sample ID
+        if id is not None:
+            prediction_files_ids.append(int(id.groups()[0]))
+    # Check prediction files
+    if not prediction_files_ids:
+        raise RuntimeError('No sample results files have been found in '
+                           'directory where samples predictions results files '
+                           'are stored.')
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Set all available samples
+    if samples_ids == 'all':
+        samples_ids = prediction_files_ids
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+    # Set number of prediction components
+    if prediction_type == 'stress_comps_paths':
+        # Probe sample prediction results
+        sample_prediction_path = \
+            os.path.join(os.path.normpath(predictions_dir),
+                         f'prediction_sample_{prediction_files_ids[0]}.pkl')
+        sample_results = load_sample_predictions(sample_prediction_path)
+        # Get number of stress components
+        n_stress_comps = sample_results['features_out'].shape[1]
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    
+        # Set number of prediction components
+        n_pred_comps = n_stress_comps
+    else:
+        raise RuntimeError('Unknown prediction data array type.')
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Initialize prediction components data
+    prediction_data_dicts = n_pred_comps*[{},]
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Loop over samples
+    for sample_id in samples_ids:
+        # Get time series discrete time
+        time_path = test_dataset[sample_id]['time_hist']
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Check if sample prediction results file is available
+        if sample_id not in prediction_files_ids:
+            raise RuntimeError(f'The prediction results file for sample '
+                               f'{sample_id} has not been found in directory: '
+                               f'\n\n{predictions_dir}')
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Set sample predictions file path
+        sample_prediction_path = \
+            os.path.join(os.path.normpath(predictions_dir),
+                         f'prediction_sample_{sample_id}.pkl')
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Load sample predictions
+        sample_results = load_sample_predictions(sample_prediction_path)
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Loop over prediction components
+        for i in range(n_pred_comps):
+            # Build sample data array
+            if prediction_type == 'stress_comps_paths':
+                # Get stress component predictions
+                stress_path = sample_results['features_out']
+                # Get stress components ground-truth
+                stress_path_target = sample_results['targets']
+                # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # Check availability of ground-truth
+                if stress_path_target is None:
+                    raise RuntimeError(f'Stress component path ground-truth '
+                                       f'is not available for sample '
+                                       f'{sample_id}.')
+                # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # Build sample data array
+                data_array = np.concatenate(
+                    (time_path.reshape((-1, 1)),
+                     stress_path_target[:, i].reshape((-1, 1)),
+                     stress_path[:, i].reshape((-1, 1))), axis=1)
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            # Assemble prediction component sample data
+            prediction_data_dicts[i][sample_id] = data_array
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    return prediction_data_dicts
