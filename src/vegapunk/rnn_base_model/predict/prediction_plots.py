16edcafd6c588944eecbf28375416727ef516506
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2024-05-10 10:32:44 +0100

    Extended time series predictions to handle general uncertainty quantification.

--- src/vegapunk/rnn_base_model/predict/prediction_plots.py
+++ src/vegapunk/rnn_base_model/predict/prediction_plots.py
@@ -26,6 +26,7 @@ def plot_time_series_prediction(prediction_sets, is_reference_data=False,
                                 x_label='Time', y_label='Value',
                                 is_normalize_data=False,
                                 is_uncertainty_quantification=False,
+                                range_type='min-max',
                                 filename='time_series_prediction',
                                 save_dir=None, is_save_fig=False,
                                 is_stdout_display=False, is_latex=False):
@@ -36,8 +37,9 @@ def plot_time_series_prediction(prediction_sets, is_reference_data=False,
     prediction_sets : dict
         One or more time series prediction processes, where each process
         (key, str) is stored as a data array (item, numpy.ndarray(2d)) of shape
-        (sequence_length, 2) as follows: data_array[:, 0] stores the time
-        series time, data_array[:, 1] stores the time series prediction.
+        (sequence_length, 1 + n_predictions) as follows: data_array[:, 0]
+        stores the time series time, data_array[:, 1:] stores the time series
+        predictions.
     is_reference_data : bool, default=False
         If True, then the first prediction process is assumed to be the
         reference and is formatted independently (black, dashed, on top).
@@ -49,9 +51,12 @@ def plot_time_series_prediction(prediction_sets, is_reference_data=False,
         Normalize time and predictions data to the range [0, 1].
     is_uncertainty_quantification: bool, default=False
         If True, then plot the prediction processes range of time series
-        predictions for each time. Assumes the same time sequence length and
-        concatenates all the different prediction processes. Data labels are
-        suppressed.
+        predictions for each time. Assumes the same time series time for all
+        the different prediction processes.
+    range_type : {'min-max', 'mean-std', None}, default='min-max'
+        Type of range used to plot the range of time series predictions for
+        each time. If None, only the mean is plotted. Only effective if
+        is_uncertainty_quantification is set to True.
     filename : str, default='time_series_prediction'
         Figure name.
     save_dir : str, default=None
@@ -73,76 +78,66 @@ def plot_time_series_prediction(prediction_sets, is_reference_data=False,
                   for x in prediction_sets.values()]):
         raise RuntimeError('Prediction processes must be provided as a dict '
                            'where each process (key, str) is stored as a '
-                           'numpy.ndarray of shape (sequence_length, 2).')
-    elif not all([x.shape[1] == 2
-                  for x in prediction_sets.values()]):
+                           'numpy.ndarray of shape '
+                           '(sequence_length, 1 + n_predictions).')
+    elif not all([x.shape[1] >= 2 for x in prediction_sets.values()]):
         raise RuntimeError('Prediction processes must be provided as a dict '
                            'where each process (key, str) is stored as a '
-                           'numpy.ndarray of shape (sequence_length, 2).')
+                           'numpy.ndarray of shape '
+                           '(sequence_length, 1 + n_predictions).')
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Get number of prediction processes
-    n_processes = len(prediction_sets.keys())
-    # Get maximum sequence length
-    max_sequence_len = max([x.shape[0] for x in prediction_sets.values()])
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Initialize data array and data labels
-    data_xy = np.full((max_sequence_len, 2*n_processes), fill_value=None)
-    data_labels = []
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Loop over prediction processes
-    for i, (key, val) in enumerate(prediction_sets.items()):
-        # Get prediction process sequence length
-        n_time = val.shape[0]
+    # Build prediction plot data
+    if is_uncertainty_quantification:        
+        # Initialize prediction data list and data labels
+        data_xy_list = []
+        data_labels = []
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        # Assemble prediction process data
-        data_xy[:n_time, 2*i] = val[:, 0]
-        data_xy[:n_time, 2*i + 1] = val[:, 1]
+        # Loop over prediction processes
+        for i, (key, val) in enumerate(prediction_sets.items()):
+            # Assemble prediction process data
+            data_xy_list.append(val)
+            # Assemble prediction process label
+            data_labels.append(key)       
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        # Assemble prediction process label
-        data_labels.append(key)
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Normalize prediction data
-    if is_normalize_data:
-        # Get minimum and maximum prediction values
-        min_pred = np.min(data_xy[:, 1::2])
-        max_pred = np.max(data_xy[:, 1::2])
-        # Normalize data array
-        for i in range(n_processes):
+        # Normalize prediction data
+        if is_normalize_data:
+            raise RuntimeError('Not implemented.')
+    else:
+        # Get number of prediction processes
+        n_processes = len(prediction_sets.keys())
+        # Get maximum sequence length
+        max_sequence_len = max([x.shape[0] for x in prediction_sets.values()])
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Initialize data array and data labels
+        data_xy = np.full((max_sequence_len, 2*n_processes), fill_value=None)
+        data_labels = []
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Loop over prediction processes
+        for i, (key, val) in enumerate(prediction_sets.items()):
             # Get prediction process sequence length
             n_time = val.shape[0]
-            # Normalize time
-            data_xy[:n_time, 2*i] = np.linspace(0, 1.0, num=n_time)
-            # Normalize predictions
-            data_xy[:n_time, 2*i+1] = \
-                (data_xy[:, 2*i+1] - min_pred)/(max_pred - min_pred)
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Restructure prediction data (uncertainty quantification)
-    if is_uncertainty_quantification:
-        # Check if all processes share the same sequence length
-        if len(set([x.shape[0] for x in prediction_sets.values()])) != 1:
-            raise RuntimeError('All prediction processes must share the same '
-                               'sequence length for uncertainy quantification '
-                               'plot.')
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            # Assemble prediction process data
+            data_xy[:n_time, 2*i] = val[:, 0]
+            data_xy[:n_time, 2*i + 1] = val[:, 1]
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            # Assemble prediction process label
+            data_labels.append(key)
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        # Initialize prediction data list
-        data_xy_list = []
-        # Get time sequence from the first prediction process
-        x_data = data_xy[:, 0].reshape(-1, 1)
-        # Assemble reference data
-        if is_reference_data:
-            # Get reference prediction data
-            y_data = data_xy[:, 1].reshape(-1, 1)
-            # Assemble reference prediction data
-            data_xy_list.append(np.concatenate((x_data, y_data), axis=1))
-        # Concatenate all prediction processes time series predictions
-        if is_reference_data:
-            y_data = np.delete(data_xy[:, 2:], np.s_[0::2], axis=1)
-        else:
-            y_data = np.delete(data_xy[:, 0:], np.s_[0::2], axis=1)
-        # Assemble prediction data
-        data_xy_list.append(np.concatenate((x_data, y_data), axis=1))
-        # Suppress data labels
-        data_labels = None
+        # Normalize prediction data
+        if is_normalize_data:
+            # Get minimum and maximum prediction values
+            min_pred = np.min(data_xy[:, 1::2])
+            max_pred = np.max(data_xy[:, 1::2])
+            # Normalize data array
+            for i in range(n_processes):
+                # Get prediction process sequence length
+                n_time = val.shape[0]
+                # Normalize time
+                data_xy[:n_time, 2*i] = np.linspace(0, 1.0, num=n_time)
+                # Normalize predictions
+                data_xy[:n_time, 2*i+1] = \
+                    (data_xy[:, 2*i+1] - min_pred)/(max_pred - min_pred)
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Set axes limits
     if is_normalize_data:
@@ -159,7 +154,7 @@ def plot_time_series_prediction(prediction_sets, is_reference_data=False,
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Plot time series predictions
     if is_uncertainty_quantification:
-        figure, _ = plot_xny_data(data_xy_list, range_type='mean-std',
+        figure, _ = plot_xny_data(data_xy_list, range_type=range_type,
                                   is_error_bar=False,
                                   is_error_shading=True,
                                   data_labels=data_labels,
