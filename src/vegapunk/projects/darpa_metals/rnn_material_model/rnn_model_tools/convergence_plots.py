9ad8526b5a27da9191649a241bc5ade7ff22b001
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2024-05-10 10:57:34 +0100

    Implemented time series predictions convergence plots for uncertainty quantification.

--- src/vegapunk/projects/darpa_metals/rnn_material_model/rnn_model_tools/convergence_plots.py
+++ src/vegapunk/projects/darpa_metals/rnn_material_model/rnn_model_tools/convergence_plots.py
@@ -7,11 +7,13 @@ plot_prediction_loss_convergence
 plot_best_parameters_convergence
     Plot best state parameters versus training data set size.
 plot_time_series_convergence
-    Plot multiple models time series predictions versus ground-truth.
+    Plot time series predictions versus ground-truth.
 plot_prediction_loss_convergence_uq
     Plot average prediction loss versus training data set size.
 plot_best_parameters_convergence_uq
     Plot best state parameters versus training data set size.
+plot_time_series_convergence_uq
+    Plot time series predictions versus ground-truth.
 """
 #
 #                                                                       Modules
@@ -42,7 +44,8 @@ __status__ = 'Planning'
 # =============================================================================
 #
 # =============================================================================
-def plot_prediction_loss_convergence(training_dirs, predictions_dirs,
+def plot_prediction_loss_convergence(models_base_dirs, training_dirs,
+                                     predictions_dirs,
                                      filename='testing_loss_convergence',
                                      save_dir=None, is_save_fig=False,
                                      is_stdout_display=False, is_latex=True):
@@ -50,6 +53,8 @@ def plot_prediction_loss_convergence(training_dirs, predictions_dirs,
     
     Parameters
     ----------
+    models_base_dirs : tuple[str]
+        Base directory where each model is stored.
     training_dirs : tuple[str]
         Directory where each model training data set is stored.
     predictions_dirs : tuple[str]
@@ -70,7 +75,7 @@ def plot_prediction_loss_convergence(training_dirs, predictions_dirs,
         processed to remove $(...)$ enclosure.
     """
     # Get number of models
-    n_models = len(training_dirs)
+    n_models = len(models_base_dirs)
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Initialize training data set sizes
     training_sizes = []
@@ -252,17 +257,19 @@ def plot_best_parameters_convergence(
         # Close plot
         plt.close('all')
 # =============================================================================
-def plot_time_series_convergence(training_dirs, testing_dirs,
+def plot_time_series_convergence(models_base_dirs, training_dirs, testing_dirs,
                                  predictions_dirs, prediction_types,
                                  plot_type='time_series_scatter',
                                  samples_ids='all',
                                  filename='time_series_convergence',
                                  save_dir=None, is_save_fig=False,
                                  is_stdout_display=False, is_latex=True):
-    """Plot multiple models time series predictions versus ground-truth.
+    """Plot time series predictions versus ground-truth.
     
     Parameters
     ----------
+    models_base_dirs : tuple[str]
+        Base directory where each model is stored.
     training_dirs : tuple[str]
         Directory where each model training data set is stored.
     testing_dirs : tuple[str]
@@ -294,7 +301,7 @@ def plot_time_series_convergence(training_dirs, testing_dirs,
         raise RuntimeError('Unknown time series plot type.')
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Get number of models
-    n_models = len(training_dirs)
+    n_models = len(models_base_dirs)
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Initialize training data set sizes
     training_sizes = []
@@ -583,7 +590,7 @@ def plot_prediction_loss_convergence_uq(models_base_dirs, training_dirs,
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Plot data
     figure, _ = scatter_xy_data(
-        data_xy, is_error_bar=True, range_type='mean-std', x_lims=x_lims,
+        data_xy, is_error_bar=True, range_type='min-max', x_lims=x_lims,
         y_lims=y_lims, x_label=x_label, y_label=y_label, x_scale='log',
         y_scale='log', is_latex=is_latex)
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -717,3 +724,176 @@ def plot_best_parameters_convergence_uq(
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Close plot
         plt.close('all')
+# =============================================================================
+def plot_time_series_convergence_uq(models_base_dirs, training_dirs,
+                                    testing_dirs, predictions_dirs,
+                                    prediction_types,
+                                    plot_type='time_series_scatter',
+                                    samples_ids='all',
+                                    filename='time_series_convergence',
+                                    save_dir=None, is_save_fig=False,
+                                    is_stdout_display=False, is_latex=True):
+    """Plot time series predictions versus ground-truth.
+
+    Uncertainty quantification data accounting for different model samples
+    predictions for each training data set size is required. The corresponding
+    directory named 'uncertainty_quantification' should exist in each model
+    base directory.
+
+    Parameters
+    ----------
+    models_base_dirs : tuple[str]
+        Base directory where each model is stored.
+    training_dirs : tuple[str]
+        Directory where each model training data set is stored.
+    testing_dirs : tuple[str]
+        Directory where each model testing data set is stored.
+    predictions_dirs : tuple[str]
+        Directory where each model samples predictions results files are
+        stored.
+    plot_type : {'time_series_scatter', 'time_series_path'}, \
+                default='time_series_scatter'
+        Time series plot type.
+    samples_ids : {'all', list[int]}, default='all'
+        Samples IDs whose prediction data is plotted.
+    filename : str, default='prediction_path_convergence'
+        Figure name.
+    save_dir : str, default=None
+        Directory where figure is saved. If None, then figure is saved in
+        current working directory.
+    is_save_fig : bool, default=False
+        Save figure.
+    is_stdout_display : bool, default=False
+        True if displaying figure to standard output device, False otherwise.
+    is_latex : bool, default=False
+        If True, then render all strings in LaTeX. If LaTex is not available,
+        then this option is silently set to False and all input strings are
+        processed to remove $(...)$ enclosure.
+    """    
+    # Check time series plot type
+    if plot_type not in ('time_series_scatter', 'time_series_path'):
+        raise RuntimeError('Unknown time series plot type.')
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Get number of models
+    n_models = len(models_base_dirs)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Initialize training data set sizes
+    training_sizes = []
+    # Loop over training data sets
+    for train_dir in training_dirs:
+        # Get training data set file path
+        regex = (r'^ss_paths_dataset_n[0-9]+.pkl$',)
+        is_file_found, train_dataset_file_path = \
+            find_unique_file_with_regex(train_dir, regex)
+        # Check training data set file
+        if not is_file_found:
+            raise RuntimeError(f'Training data set file has not been found  '
+                               f'in data set directory:\n\n'
+                               f'{train_dir}')
+        # Get training data set size
+        training_sizes.append(len(load_dataset(train_dataset_file_path)))
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Loop over prediction types
+    for prediction_type, prediction_comps in prediction_types.items():
+        # Initialize models time series predictions data
+        models_prediction_data = []
+        # Initialize testing data set sizes
+        testing_sizes = []
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+        # Loop over testing directories
+        for i, test_dir in enumerate(testing_dirs):
+            # Get testing data set file path
+            regex = (r'^ss_paths_dataset_n[0-9]+.pkl$',)
+            is_file_found, test_dataset_file_path = \
+                find_unique_file_with_regex(test_dir, regex)
+            # Check testing data set file
+            if not is_file_found:
+                raise RuntimeError(f'Testing data set file has not been found '
+                                   f'in data set directory:\n\n'
+                                   f'{test_dir}')
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            # Get testing data set size
+            testing_sizes.append(len(load_dataset(test_dataset_file_path)))
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            # Get corresponding prediction directory
+            pred_dir = predictions_dirs[i]
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            # Build times series predictions components data arrays
+            if plot_type == 'time_series_scatter':
+                # Build prediction components data arrays
+                raise RuntimeError('Not implemented.')
+            elif plot_type == 'time_series_path':
+                # Build prediction components data arrays for each sample
+                prediction_data_arrays = build_time_series_predictions_data(
+                    test_dataset_file_path, pred_dir,
+                    prediction_type=prediction_type,
+                    samples_ids=samples_ids,
+                    is_uncertainty_quantification=True)
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            # Store model time series prediction components data arrays
+            models_prediction_data.append(prediction_data_arrays)
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Check testing data sets
+        if len(set(testing_sizes)) != 1:
+            raise RuntimeError('Expecting the same testing data set but got '
+                               'testing data sets with different sizes.')
+        # Get testing data set size
+        testing_size = testing_sizes[0]
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Loop over prediction type components
+        for j, prediction_comp in enumerate(prediction_comps):
+            # Plot model times series prediction component
+            if plot_type == 'time_series_scatter':
+                raise RuntimeError('Not implemented.')
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            elif plot_type == 'time_series_path':
+                # Set all available samples
+                if samples_ids == 'all':
+                    samples_ids = range(testing_size)
+                # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                # Loop over samples
+                for sample_id in samples_ids:
+                    # Initialize prediction processes
+                    prediction_sets = {}
+                    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    # Assemble discrete time path and ground-truth path by
+                    # probing the first model data                    
+                    prediction_sets['Ground-Truth'] = \
+                        models_prediction_data[0][j][str(sample_id)][:, [0, 1]]
+                    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    # Loop over models
+                    for i in range(n_models):
+                        # Get model time series predictions data
+                        model_prediction_data = models_prediction_data[i]
+                        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                        # Set prediction label
+                        label = 'N = ' + f'{training_sizes[i]}'
+                        # Assemble sample prediction data (remove ground-truth)
+                        prediction_sets[label] = np.delete(
+                            model_prediction_data[j][str(sample_id)], 1,
+                            axis=1)
+                    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    # Set prediction type label
+                    if prediction_type == 'stress_comps':
+                        y_label = 'Stress (MPa)'
+                    elif prediction_type == 'acc_p_strain':
+                        y_label = 'Accumulated plastic strain'
+                    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    # Plot model times series predictions
+                    plot_time_series_prediction(
+                        prediction_sets, is_reference_data=True,
+                        x_label='Time', y_label=y_label,
+                        is_normalize_data=False,
+                        is_uncertainty_quantification=True,
+                        range_type='min-max',
+                        filename=(filename + f'_{prediction_comp}'
+                                  + f'_path_sample_{sample_id}'),
+                        save_dir=save_dir, is_save_fig=is_save_fig,
+                        is_stdout_display=is_stdout_display, is_latex=is_latex)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Display figure
+    if is_stdout_display:
+        plt.show()
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Close plot
+    plt.close('all')
