0e454aaa122580242b6ad3dbf407664fd441290e
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2024-08-21 10:31:51 -0400

    Successful implementation of nested vmaps in testing script.

--- src/vegapunk/testing_utilities/leveraging_torch_vmap.py
+++ src/vegapunk/testing_utilities/leveraging_torch_vmap.py
@@ -5,7 +5,7 @@ import time
 # Third-party
 import torch
 # =============================================================================
-# Summary:
+# Summary: Testing vectorized mapping (vmap) to minimize memory costs
 # =============================================================================
 class ConstitutiveModel:
     def __init__(self):
@@ -61,6 +61,20 @@ class StructureMaterialState:
     def update_element_state(self, elem_id, element_state):
         # Update element material constitutive state variables
         self._elements_state[str(elem_id)] = element_state
+# ============================================================================= 
+def compute_loss(elements_state_hist):
+    # Initialize loss
+    loss = 0
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Loop over elements
+    for _, element_state_hist in elements_state_hist.items():
+        # Loop over discrete time
+        for element_state in element_state_hist:
+            # Loop over Gauss integration points
+            for _, tensor in element_state.items():
+                loss += torch.sum(tensor)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    return loss    
 # =============================================================================  
 class MaterialModelFinder(torch.nn.Module):
     def __init__(self):
@@ -75,7 +89,6 @@ class MaterialModelFinder(torch.nn.Module):
         self._set_model_parameters()
     # -------------------------------------------------------------------------
     def _set_model_parameters(self):
-        """Set model parameters (collect material models parameters)."""
         # Initialize parameters
         self._model_parameters = torch.nn.ParameterDict({})
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -99,6 +112,9 @@ class MaterialModelFinder(torch.nn.Module):
         # Get elements material
         elements_material = specimen_material_state.get_elements_material()
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Initialize elements state variable history
+        elements_state_hist = {}
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Loop over elements
         for i in range(n_elem):
             # Get element label
@@ -117,18 +133,28 @@ class MaterialModelFinder(torch.nn.Module):
             # Update element material constitutive state variables
             specimen_material_state.update_element_state(
                 elem_id, element_state_hist[-1])
-            
-            print(f'Processing element {elem_id}/{n_elem + 1}')
-            
-            print(specimen_material_state._elements_state)
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            # Store element state variable history
+            elements_state_hist[str(elem_id)] = element_state_hist
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Compute loss
+        loss = compute_loss(elements_state_hist)
+        # Display loss
+        print(f'> Loss: {loss}')
+        # Compute loss gradient
+        loss.backward()
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Get element material constitutive model parameters
+        model_parameters = elements_material['1'].get_model_parameters()
+        # Get material model parameter
+        param_1 = model_parameters['param_1']
+        # Display loss gradient w.r.t material model parameter
+        print(f'> Loss gradient: {param_1.grad}')
     # -------------------------------------------------------------------------
     def compute_state_variable_hist(self, element_material, element_disp_hist,
                                     time_hist):
         # Set element number of Gauss quadrature integration points
         n_gauss = 8
-        # Set complexity of state update function (emulating complexity of
-        # computation graph)
-        state_update_complexity = ('low', 'medium', 'high')[1]
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Get time history length
         n_time = time_hist.shape[0]
@@ -147,30 +173,22 @@ class MaterialModelFinder(torch.nn.Module):
         for i in range(n_gauss):
             # Loop over discrete time
             for time_idx in range(n_time):
-                # Get element displacement
-                disp = element_disp_hist[time_idx]
+                # Get previous converged state variable
+                if time_idx == 0:
+                    state_variable_old = torch.zeros(6)
+                else:
+                    state_variable_old = \
+                        element_state_hist[time_idx - 1][str(i + 1)]
                 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                 # Compute state variable
-                if state_update_complexity == 'low':
-                    state_variable = \
-                        (param_1*disp*(i + 1)
-                         *torch.ones(6, dtype=torch.float))
-                elif state_update_complexity == 'medium':
-                    state_variable = \
-                        (param_1*disp*(i + 1)
-                         *torch.ones(6, dtype=torch.float)
-                        + (param_1**2)*disp*(i + 1)
-                        *torch.ones((3, 2), dtype=torch.float).reshape(6))
-                else:
-                    raise RuntimeError('Unknown complexity option.')
+                state_variable = \
+                    (param_1*element_disp_hist[time_idx]
+                     *torch.ones(6, dtype=torch.float)) + state_variable_old
                 # Store state variable
                 element_state_hist[time_idx][str(i + 1)] = state_variable
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         return element_state_hist
     # -------------------------------------------------------------------------
-
-
-
     def forward_sequential_element_vmap(self, n_elem, time_hist,
                                         elements_disp_hist):
         # Get specimen material state
@@ -182,86 +200,87 @@ class MaterialModelFinder(torch.nn.Module):
         # Get elements material
         elements_material = specimen_material_state.get_elements_material()
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
         # WHAT IF I HAVE DIFFERENT MATERIAL MODELS?
         element_material = elements_material['1']
-
-
-
-        # BUILD ELEMENT BATCHED INPUT TENSOR (n_elem x n_time)
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # BUILD ELEMENT BATCHED INPUT TENSOR
+        # (shape: n_elem x n_time)
         # Initialize tensor of elements displacement history
-        elements_disp_hist_tensor = torch.zeros((n_elem, n_time))
+        elements_input_data_tensor = torch.zeros((n_elem, n_time))
         # Loop over elements
         for i in range(n_elem):
             # Get element label
             elem_id = i + 1
             # Assemble element displacement history
-            elements_disp_hist_tensor[i, :] = elements_disp_hist[str(elem_id)]
-
-
+            elements_input_data_tensor[i, :] = elements_disp_hist[str(elem_id)]
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # ELEMENT FUNCTION TO BE VECTORIZED
-        # input: n_time
-        # output: n_gp x n_time x n_features
-        def func_elem(element_disp_hist):
+        # input shape: n_time
+        # output shape: n_gp x n_time x n_features
+        def func_elem(element_input_data):
             # Set element number of Gauss quadrature integration points
             n_gauss = 8
             # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             # Get time history length
-            n_time = element_disp_hist.shape[0]
+            n_time = element_input_data.shape[0]
             # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             # Get element material constitutive model parameters
             model_parameters = element_material.get_model_parameters()
             # Get material model parameter
             param_1 = model_parameters['param_1']
             # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-            # BUILD GAUSS POINT BATCHED INPUT TENSOR (n_gp x n_time)
-            gauss_shapef_input_list = [param_1*(i + 1)*element_disp_hist for i in range(n_gauss)]
-            gauss_shapef_input = torch.stack(gauss_shapef_input_list, dim=0)
-
+            # BUILD GAUSS POINT BATCHED INPUT TENSOR
+            # (shape: n_gp x n_time)
+            gausss_input_data_vlist = \
+                [element_input_data for i in range(n_gauss)]
+            gausss_input_data_tensor = \
+                torch.stack(gausss_input_data_vlist, dim=0)
             # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            
             # GAUSS POINT FUNCTION TO BE VECTORIZED
-            # input: n_time
-            # output: n_time x n_features
-            def func_gp(gauss_shapef):
-                # Initialize Gauss point data vlist
-                gauss_data_vlist = []
+            # input shape: n_time
+            # output shape: n_time x n_features
+            def func_gp(gauss_input_data):
+                # Initialize Gauss point data
+                gauss_output_data_vlist = []
                 # Loop over time
                 for t in range(n_time):
-                    #gauss_data_vlist.append(gauss_shapef[t]*param_1*torch.ones(6))
-                    gauss_data_vlist.append((param_1*gauss_shapef[t]*(i + 1)*torch.ones(6)
-                                             + (param_1**2)*gauss_shapef[t]*(i + 1)*torch.ones((3, 2)).reshape(6)))
-
-                
+                    # Get previous converged state variable
+                    if t == 0:
+                        state_variable_old = torch.zeros(6)
+                    else:
+                        state_variable_old = gauss_output_data_vlist[-1]
+                    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    # Compute state variable
+                    state_variable = state_variable_old \
+                        + param_1*gauss_input_data[t]*torch.ones(6)
+                    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    # Store state variable
+                    gauss_output_data_vlist.append(state_variable)
+                # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                 # BUILD GAUSS POINT OUTPUT TENSOR
-                # n_time x n_features
-                gauss_data = torch.stack(gauss_data_vlist, dim=0)
-                
+                # (shape: n_time x n_features)
+                gauss_data = torch.stack(gauss_output_data_vlist, dim=0)
+                # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                 return gauss_data
-
             # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-            
             # VECTORIZE GAUSS POINT FUNCTION
             vmap_gp = torch.vmap(func_gp)
-            
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             # BUILD ELEMENT OUTPUT TENSOR
-            # n_gp x n_time x n_features 
-            element_state_hist_tensor = vmap_gp(gauss_shapef_input)
-
+            # (shape: n_gp x n_time x n_features)
+            element_state_hist_tensor = vmap_gp(gausss_input_data_tensor)
             # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             return element_state_hist_tensor
-
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
         # VECTORIZE ELEMENT FUNCTION
         vfunc_elem = torch.vmap(func_elem)
-        
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # COMPUTE MESH OUTPUT TENSOR
-        # n_elem x n_gp x n_time x n_features
-        elements_state_hist_tensor = vfunc_elem(elements_disp_hist_tensor)
-
-        
+        # (shape: n_elem x n_gp x n_time x n_features)
+        elements_state_hist_tensor = vfunc_elem(elements_input_data_tensor)
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Initialize elements state variable history
+        elements_state_hist = {}
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Loop over elements
         for i in range(n_elem):
@@ -272,8 +291,9 @@ class MaterialModelFinder(torch.nn.Module):
             # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
             # Initialize element material constitutive model state variables
             # history
-            element_state_hist = [{key: None for key in range(1, n_gauss + 1)}
-                                  for _ in range(n_time)]
+            element_state_hist = \
+                [{str(key): None for key in range(1, n_gauss + 1)}
+                 for _ in range(n_time)]
             # Loop over Gauss integration points
             for j in range(n_gauss):
                 # Loop over discrete time
@@ -284,14 +304,28 @@ class MaterialModelFinder(torch.nn.Module):
             # Update element material constitutive state variables
             specimen_material_state.update_element_state(
                 elem_id, element_state_hist[-1])
-        
-
-
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            # Store element state variable history
+            elements_state_hist[str(elem_id)] = element_state_hist
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Compute loss
+        loss = compute_loss(elements_state_hist)
+        # Display loss
+        print(f'> Loss: {loss}')
+        # Compute loss gradient
+        loss.backward()
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Get element material constitutive model parameters
+        model_parameters = elements_material['1'].get_model_parameters()
+        # Get material model parameter
+        param_1 = model_parameters['param_1']
+        # Display loss gradient w.r.t material model parameter
+        print(f'> Loss gradient: {param_1.grad}')     
 # =============================================================================
 # Set number of elements
-n_elem = 100
+n_elem = 30
 # Set time history length
-n_time = 1000
+n_time = 10
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Set time history
 time_hist = torch.tensor([x for x in range(0, n_time)], dtype=torch.float)
@@ -320,7 +354,11 @@ process = psutil.Process(os.getpid())
 start_time_sec = time.time()
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Set forward propagation mode
-is_forward_vmap = False
+is_forward_vmap = True
+# Display forward propagation mode
+print(f'\nForward propagation mode: {is_forward_vmap}\n'
+      + len(f'Forward propagation mode: {is_forward_vmap}')*'-')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Forward propagation
 if is_forward_vmap:
     material_model_finder.forward_sequential_element_vmap(n_elem, time_hist,
@@ -332,10 +370,12 @@ else:
 # Compute execution time
 exec_time_sec = time.time() - start_time_sec
 # Display execution time
-print(f'Execution time: {exec_time_sec:.4f} s')
+print(f'\n> Execution time: {exec_time_sec:.4f} s')
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Get process non-swapped physical memory usage
 memory_usage = process.memory_info().rss
 memory_usage_mb = memory_usage/(1024**2)
 # Display memory usage
-print(f"Memory usage: {memory_usage_mb:.2f} MB")
+print(f'> Memory usage: {memory_usage_mb:.2f} MB')
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+print()
