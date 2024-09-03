f05cc1950921e9309bcf3d9d2c698f35aa0cd7fb
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2024-09-03 17:36:06 -0400

    Initialization file for vmap constitutive models.

---
+++ src/vegapunk/simulators/fetorch/material/models/vmap/__init__.py
@@ -0,0 +1,4 @@
+#
+#                                                                       Modules
+# =============================================================================
+from simulators.fetorch.material.models.vmap import von_mises
