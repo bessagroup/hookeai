a49de61038b9436322f98bc4b005e5d660aa9eb9
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2024-10-07 16:27:38 -0400

    Implemented hybridization model.

---
+++ src/vegapunk/hybrid_base_model/model/hybridization.py
@@ -0,0 +1,120 @@
+"""Hybridization model.
+
+Classes
+-------
+HybridizationModel(torch.nn.Module)
+    Hybridization model.
+HMAdditive(torch.nn.Module)
+    Hybridization model: Additive.
+"""
+#
+#                                                                       Modules
+# =============================================================================
+# Third-party
+import torch
+#
+#                                                          Authorship & Credits
+# =============================================================================
+__author__ = 'Bernardo Ferreira (bernardo_ferreira@brown.edu)'
+__credits__ = ['Bernardo Ferreira', ]
+__status__ = 'Planning'
+# =============================================================================
+#
+# =============================================================================
+class HybridizationModel(torch.nn.Module):
+    """Hybridization model.
+    
+    Attributes
+    ----------
+    hybridization_type : str
+        Hybridization model type.
+    hybridization_model : torch.nn.Module
+        Hybridization model.
+    
+    Methods
+    -------
+    forward(self, list_features_in)
+        Forward propagation.
+
+    """
+    def __init__(self, hybridization_type='additive'):
+        """Constructor.
+        
+        Parameters
+        ----------
+        hybridization_type : str, default='additive'
+            Hybridization model type. Default 'additive' corresponds to
+            hybridization model without lernable parameters where the outputs
+            of the different hybridized models are added to compute the hybrid
+            model output.
+        """
+        # Set hybridization model type
+        self._hybridization_type = hybridization_type
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Initialize hybridization model
+        if hybridization_type == 'additive':
+            self.hybridization_model = HMAdditive()
+        else:
+            raise RuntimeError('Unknown hybridization type.')
+    # -------------------------------------------------------------------------
+    def forward(self, list_features_in):
+        """Forward propagation.
+        
+        Parameters
+        ----------
+        list_features_in : list[torch.Tensor]
+            List of similar shaped tensors of input features stored as
+            torch.Tensor(2d) of shape (sequence_length, n_features_in) for
+            unbatched input or torch.Tensor(3d) of shape
+            (sequence_length, batch_size, n_features_in) for batched input.
+
+        Returns
+        -------
+        features_out : torch.Tensor
+            Tensor of output features stored as torch.Tensor(2d) of shape
+            (sequence_length, n_features_in) for unbatched input or
+            torch.Tensor(3d) of shape
+            (sequence_length, batch_size, n_features_in) for batched input.
+        """
+        # Forward propagation
+        features_out = self.hybridization_model(list_features_in)
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        return features_out
+# =============================================================================
+class HMAdditive(torch.nn.Module):
+    """Hybridization model: Additive.
+    
+    Add input tensors.
+
+    Methods
+    -------
+    forward(self, list_features_in)
+        Forward propagation.
+    """
+    def __init__(self):
+        """Constructor."""
+        pass
+    # -------------------------------------------------------------------------
+    def forward(self, list_features_in):
+        """Forward propagation.
+
+        Parameters
+        ----------
+        list_features_in : list[torch.Tensor]
+            List of similar shaped tensors of input features stored as
+            torch.Tensor(2d) of shape (sequence_length, n_features_in) for
+            unbatched input or torch.Tensor(3d) of shape
+            (sequence_length, batch_size, n_features_in) for batched input.
+
+        Returns
+        -------
+        features_out : torch.Tensor
+            Tensor of output features stored as torch.Tensor(2d) of shape
+            (sequence_length, n_features_in) for unbatched input or
+            torch.Tensor(3d) of shape
+            (sequence_length, batch_size, n_features_in) for batched input.
+        """
+        # Add input tensors
+        features_out = sum(list_features_in)
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        return features_out
