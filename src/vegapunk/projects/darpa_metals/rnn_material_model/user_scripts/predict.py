f205068be1617e47df8d170e5dfc1520c1b7493e
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2024-11-05 16:17:47 -0500

    Minor update.

--- src/vegapunk/projects/darpa_metals/rnn_material_model/user_scripts/predict.py
+++ src/vegapunk/projects/darpa_metals/rnn_material_model/user_scripts/predict.py
@@ -193,6 +193,23 @@ def perform_model_prediction(predict_directory, dataset_file_path,
     # Generate plots of model predictions
     generate_prediction_plots(dataset_file_path, predict_subdir)
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Set remove sample prediction files flag
+    is_remove_sample_prediction = True
+    # Remove sample prediction files
+    if is_remove_sample_prediction:
+        # Set sample prediction file regex
+        sample_regex = re.compile(r'^prediction_sample_\d+\.pkl$')
+        # Walk through prediction set directory recursively
+        for root, _, files in os.walk(predict_subdir):
+            # Loop over prediction set directory files
+            for file in files:
+                # Remove sample prediction file
+                if sample_regex.match(file):
+                    # Set sample prediction file path
+                    sample_file_path = os.path.join(root, file)
+                    # Remove sample prediction file
+                    os.remove(sample_file_path)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     return predict_subdir
 # =============================================================================
 def generate_prediction_plots(dataset_file_path, predict_subdir):
