fb7b268a23d6e8747958ef796b63e142c3a23998
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2024-11-05 09:43:23 -0500

    Enrich script to perform tasks on case study directories.

--- src/vegapunk/testing_utilities/process_case_study_dirs.py
+++ src/vegapunk/testing_utilities/process_case_study_dirs.py
@@ -10,20 +10,21 @@ if root_dir not in sys.path:
 import os
 import re
 import shutil
-# Local
-from ioput.iostandard import make_directory
 # =============================================================================
 # Summary: Process case study directories and perform some task
 # =============================================================================
 # Set available tasks
-available_tasks = {'1': 'remove_plots_folders'}
+available_tasks = {'1': 'remove_plots_dirs',
+                   '2': 'remove_sample_prediction_files'}
 # Set task
 task = available_tasks['1']
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Set source directory
 src_dir = ('/home/bernardoferreira/Documents/brown/projects/darpa_project/'
            '7_local_hybrid_training/case_learning_drucker_prager_pressure/'
-           '0_datasets/datasets_drucker_prager_10deg/proportional/no_plots')
+           '0_datasets/datasets_drucker_prager_10deg/proportional_2cycle/'
+           'no_plots')
+src_dir = ('/home/bernardoferreira/Documents/brown/projects/darpa_project/test')
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Initialize case study directories
 case_study_dirs = []
@@ -49,14 +50,14 @@ for case_study_dir in case_study_dirs:
     l1_paths = [os.path.join(os.path.normpath(case_study_dir), x)
                 for x in l1_names]
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Perform task
-    if task == 'remove_plots_folders':
+    # Task: Remove plot directory from data set directories
+    if task == 'remove_plots_dirs':
         # Set data sets types
         dataset_types = ('1_training_dataset', '2_validation_dataset',
                          '5_testing_id_dataset', '6_testing_od_dataset')
         # Loop over case study directories
         for i, l1_name in enumerate(l1_names):
-            # Check if data set folder
+            # Check if data set directory
             if l1_name not in dataset_types:
                 continue
             # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -65,6 +66,29 @@ for case_study_dir in case_study_dirs:
             # Remove existing plots directory
             if os.path.isdir(plots_dir):
                 shutil.rmtree(plots_dir)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Task: Remove sample prediction files
+    elif task == 'remove_sample_prediction_files':
+        # Set sample prediction file regex
+        sample_regex = re.compile(r'^prediction_sample_\d+\.pkl$')
+        # Set prediction folder name
+        prediction_dirname = '7_prediction'
+        # Set prediction directory
+        prediction_dir = \
+            os.path.join(os.path.normpath(case_study_dir), prediction_dirname)
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Check prediction directory
+        if prediction_dir in l1_paths:
+            # Walk through prediction directory recursively
+            for root, dirs, files in os.walk(prediction_dir):
+                # Loop over directory files
+                for file in files:
+                    # Remove sample prediction file
+                    if sample_regex.match(file):
+                        # Set sample prediction file path
+                        sample_file_path = os.path.join(root, file)
+                        # Remove sample prediction file
+                        os.remove(sample_file_path)
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     else:
         raise RuntimeError('Unknown task.')
