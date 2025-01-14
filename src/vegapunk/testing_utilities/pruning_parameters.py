173d83a5329d4865b7dd9bbc337ae044d627d1b0
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2025-01-14 09:20:24 +0000

    Updated script to assess data set pruning parameters.

--- src/vegapunk/testing_utilities/pruning_parameters.py
+++ src/vegapunk/testing_utilities/pruning_parameters.py
@@ -26,23 +26,24 @@ n_full = 1000
 split_sizes = {'training': 0.8, 'validation': 0.2}
 # Set pruning training, validation and testing split sizes
 pruning_split_sizes = {'training': 0.7, 'validation': 0.2, 'testing': 0.1}
-# Set small value
-delta = 1e-10
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Set number of pruning steps
-n_prun_step = 10
-# Set minimum data set size ratio
-min_size_ratio = 0.05
+# Set number of pruned samples
+n_prun_sample = 10
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Compute number of pruned samples
-n_prun_sample = \
-    int(np.floor((1.0 - min_size_ratio)*(n_full/n_prun_step) + delta))
+# Set pruning termination criteria parameters:
+# Set minimum development data set size
+n_dev_min = 10
+# Set minimum pruning testing data set size
+n_prun_test_min = 10
+# Set maximum pruned samples testing ratio
+prun_test_ratio_max = 0.5
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Display
 print(f'\n> Full data set size: {n_full}')
-print(f'\n> Number of pruning steps: {n_prun_step}')
-print(f'\n> Number of training procedures: {2*n_prun_step + 1}')
-print(f'\n> Number of testing procedures: {n_prun_step}')
+print(f'\n> Number of pruned samples (per pruning step): {n_prun_sample}')
+print(f'\n> Minimum development data set size: {n_dev_min}')
+print(f'\n> Minimum pruning testing data set size: {n_prun_test_min}')
+print(f'\n> Maximum pruned samples testing ratio: {prun_test_ratio_max}')
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Display
 print(f'\n> Data set pruning iterations:')
@@ -52,18 +53,20 @@ print(f'\n{"iteration":>11s} '
       f'{"validation":>13s} {"%full":>6s} '
       f'{"unused":>9s} {"%full":>6s}')
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Compute number of iterations
-n_iter = n_prun_step + 1
+# Initialize pruning iterative data
+pruning_iter_data = []
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Initialize plot data
-data_xy = np.zeros((n_iter, 6))
-# Set data labels
-data_labels = ('Development', 'Training', 'Validation')
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Initialize pruning iteration counter
+iter = 0
+# Initialize pruning termination flag
+is_keep_pruning = True
 # Loop over iterations
-for iter in range(n_iter):
+while is_keep_pruning:
     # Compute development data set size (training + validation)
-    n_dev = n_full - iter*n_prun_sample
+    if iter == 0:
+        n_dev = n_full
+    else:
+        n_dev = n_dev - n_prun_sample
     # Compute ratio
     ratio_dev = (n_dev/n_full)*100
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -73,6 +76,28 @@ for iter in range(n_iter):
     n_prun_valid = int(np.floor(pruning_split_sizes['validation']*n_dev))
     # Compute pruning testing data set size
     n_prun_test = int(np.floor(pruning_split_sizes['testing']*n_dev))
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Check pruning termination criteria
+    termination_status = None
+    if n_dev <= n_dev_min:
+        termination_status = 'Minimum development data set size reached.'
+    elif n_prun_test < n_prun_test_min:
+        termination_status = 'Minimum pruning testing data set size reached.'
+    elif n_prun_sample/n_prun_test > prun_test_ratio_max:
+        termination_status = 'Maximum pruned samples testing ratio reached.'
+    # Terminate pruning
+    if termination_status is not None:
+        # Display termination status
+        print(f'\n> Termination status: {termination_status}')
+        # Terminate pruning
+        is_keep_pruning = False
+        continue
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Display pruning step
+    if iter > 0:
+        print(35*' '
+              + f'(pruning step: '
+              f'T{n_prun_train}|V{n_prun_valid}|T{n_prun_test})')
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Compute training data set size
     n_train = int(np.floor(split_sizes['training']*n_dev))
@@ -84,18 +109,31 @@ for iter in range(n_iter):
     n_unused = iter*n_prun_sample
     ratio_unused = (n_unused/n_full)*100
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Display
+    # Display pruning iteration
     print(f'{iter:>11d} '
           f'{n_dev:>14d} {ratio_dev:>6.1f} '
           f'{n_train:>11d} {ratio_train:>6.1f} '
           f'{n_valid:>13d} {ratio_valid:>6.1f} '
           f'{n_unused:>9d} {ratio_unused:>6.1f}')
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Append plot data
-    data_xy[iter, :] = \
-        [iter, ratio_dev, iter, ratio_train, iter, ratio_valid]
+    # Store pruning iterative data
+    pruning_iter_data.append(
+        [iter, ratio_dev, iter, ratio_train, iter, ratio_valid])
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Update pruning iteration counter
+    iter += 1
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 print()
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Get number of pruning iterations
+n_iter = len(pruning_iter_data)
+# Initialize plot data
+data_xy = np.zeros((n_iter, 6))
+# Assemble plot data
+for iter in range(n_iter):
+    data_xy[iter, :] = pruning_iter_data[iter]
+# Set data labels
+data_labels = ('Development', 'Training', 'Validation')
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Plot data set iterations
 figure, _ = plot_xy_data(data_xy,
@@ -104,5 +142,6 @@ figure, _ = plot_xy_data(data_xy,
                          y_label='\% Full data set',
                          marker='o',
                          is_latex=True)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Display plot
 plt.show()
