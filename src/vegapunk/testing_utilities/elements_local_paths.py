b1df4046e263dd8a0006e9ae551036facff7c0f9
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2025-02-18 15:35:39 -0500

    Implemented method to get elements local paths indices.

---
+++ src/vegapunk/testing_utilities/elements_local_paths.py
@@ -0,0 +1,39 @@
+# =============================================================================
+# Summary: Get elements local paths indices
+# =============================================================================
+def get_element_local_paths_idx(element_id, n_gauss):
+    """Get finite element local paths global indices.
+    
+    Parameters
+    ----------
+    element_id : int
+        Element label. Elements labels must be within the range of
+        1 to n_elem (included).
+    n_gauss : int
+        Number of Gauss quadrature integration points (per element).
+        
+    Returns
+    -------
+    local_paths_idx : tuple[int]
+        Element local paths indices.
+    """
+    # Compute element local paths indices
+    local_paths_idx = \
+        tuple(range((element_id - 1)*n_gauss, element_id*n_gauss))
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    return local_paths_idx
+# =============================================================================
+if __name__ == "__main__":
+    # Set element label
+    element_id = 6
+    # Set number of Gauss points per element
+    n_gauss = 8
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Get element local paths indices
+    local_paths_idx = get_element_local_paths_idx(element_id, n_gauss)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Display results
+    print(f'\nElement label:          {element_id}')
+    print(f'\nNumber of Gauss points: {n_gauss}')
+    print(f'\nLocal paths indices:    {local_paths_idx}')
+    print()
