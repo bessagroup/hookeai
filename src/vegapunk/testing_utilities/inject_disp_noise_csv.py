0a356ea7c6e31da716fac4bad0e6bcfc8670394d
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2025-04-16 16:54:55 -0400

    Update synthetic displacements noise.

--- src/vegapunk/testing_utilities/inject_disp_noise_csv.py
+++ src/vegapunk/testing_utilities/inject_disp_noise_csv.py
@@ -26,6 +26,8 @@ def inject_displacements_noise(csv_data_dir, noise_parameters):
     ----------
     csv_data_dir : str
         Directory with '.csv' data files.
+    noise_parameters : dict
+        Synthetic noise parameters.
     """
     # Check if data files directory exists
     if not os.path.exists(csv_data_dir):
@@ -85,14 +87,12 @@ def inject_displacements_noise(csv_data_dir, noise_parameters):
     for t in range(1, n_time):
         # Get current and previous time step data files
         data_file_path = data_file_paths[t]
-        data_file_path_old = data_file_paths[t - 1]
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Load data
         df = pandas.read_csv(data_file_path)
-        df_old = pandas.read_csv(data_file_path_old)
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Check data
-        if df.shape[1] != 10 or df_old.shape[1] != 10:
+        if df.shape[1] != 10:
             raise RuntimeError(f'Expecting data frame to have 10 columns, but '
                                f'{df.shape[1]} were found. \n\n'
                                f'Expected columns: NODE | X1 X2 X3 | '
@@ -100,18 +100,19 @@ def inject_displacements_noise(csv_data_dir, noise_parameters):
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Get node displacements
         nodes_disps_mesh = np.array(df.values)[:, 4:7]
-        nodes_disps_mesh_old = np.array(df_old.values)[:, 4:7]
-        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        # Compute incremental node displacements
-        nodes_inc_disps_mesh = nodes_disps_mesh - nodes_disps_mesh_old
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Generate noise
-        noise = noise_generator.generate_noise_path(nodes_inc_disps_mesh)
-        # Inject noise into incremental node displacements
-        nodes_inc_disps_mesh_noisy = (1.0 + noise)*nodes_inc_disps_mesh
-        # Compute noisy node displacements
-        nodes_disps_mesh_noisy = \
-            nodes_disps_mesh_old + nodes_inc_disps_mesh_noisy
+        noise = noise_generator.generate_noise_path(nodes_disps_mesh)
+        # Inject noise into node displacements
+        nodes_disps_mesh_noisy = nodes_disps_mesh + noise
+        # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        # Set noise injection threshold
+        is_noise_threshold = False
+        # Threshold noise injection
+        if is_noise_threshold:
+            nodes_disps_mesh_noisy = np.where(np.abs(nodes_disps_mesh) < 1e-10,
+                                              nodes_disps_mesh,
+                                              nodes_disps_mesh_noisy)
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Update data with noisy node displacements
         df.iloc[:, 4:7] = nodes_disps_mesh_noisy
@@ -125,20 +126,25 @@ def inject_displacements_noise(csv_data_dir, noise_parameters):
 # =============================================================================
 if __name__ == "__main__":
     # Set directory with data files
-    csv_data_dir = ('/home/bernardoferreira/Desktop/test_sample_memory/'
-                    'specimen_history_data')
+    csv_data_dir = ('/home/bernardoferreira/Documents/brown/projects/'
+                    'darpa_paper_examples/global/tensile_dogbone_von_mises/'
+                    'hexa8_n6493_e1200/1_discover_rc_von_mises/'
+                    'E_v_s0_a_b/noisy_displacements/noiseless/'
+                    'material_model_finder/0_simulation/specimen_history_data')
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # Set measurement resolution
+    measurement_resolution = 0.27e-3
     # Initialize noise cases
-    gaussian_std_values = [0.01, 0.025, 0.05, 0.1]
+    uniform_amp_values = [measurement_resolution,]
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Loop over noise cases
-    for gaussian_std in gaussian_std_values:
+    for uniform_amp in uniform_amp_values:
         # Set noise parameters
         noise_parameters = {
-            'distribution': 'gaussian',
-            'std': gaussian_std,
-            'dirname': f'noisy_disps_gaussian_std_'
-                       f'{gaussian_std}'.replace('.', 'd')}
+            'distribution': 'uniform',
+            'amp': uniform_amp,
+            'dirname': f'noisy_disps_uniform_amp_'
+                       f'{uniform_amp}'.replace('.', 'd')}
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # Inject synthetic noise into structure nodes displacements
         inject_displacements_noise(csv_data_dir, noise_parameters)
