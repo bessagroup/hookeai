f11ef1a38d410f527a35aa9febbb9f17e13d853b
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2025-07-28 10:48:35 -0400

    Removed forgotten print statements.

--- src/vegapunk/model_architectures/materials/process_predictions.py
+++ src/vegapunk/model_architectures/materials/process_predictions.py
@@ -352,10 +352,6 @@ def build_time_series_predictions_data(dataset_file_path, predictions_dir,
         n_pred_comps = n_strain_comps
     else:
         raise RuntimeError('Unknown prediction data array type.')
-    
-    print(prediction_type)
-    print(n_pred_comps)
-    
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Initialize prediction components data
     prediction_data_arrays = [{} for _ in range(n_pred_comps)]
