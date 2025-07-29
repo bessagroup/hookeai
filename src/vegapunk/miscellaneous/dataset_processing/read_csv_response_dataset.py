4de0e4564f9fb664f32971d649c97b0104a23897
Bernardo Ferreira <bernardoferreiraacc@gmail.com>
2025-07-29 07:48:11 -0400

    Small fix.

--- src/vegapunk/miscellaneous/dataset_processing/read_csv_response_dataset.py
+++ src/vegapunk/miscellaneous/dataset_processing/read_csv_response_dataset.py
@@ -7,7 +7,7 @@ import sys
 import pathlib
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Add project root directory to sys.path
-root_dir = str(pathlib.Path(__file__).parents[1])
+root_dir = str(pathlib.Path(__file__).parents[2])
 if root_dir not in sys.path:
     sys.path.insert(0, root_dir)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
